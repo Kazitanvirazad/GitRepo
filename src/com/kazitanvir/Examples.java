487f96bfcd27d6808ed3238ba92fbde8ef30157d
kazi tanvir azad <tanvir.kaziazad@gmail.com>
2022-02-19 19:53:07 +0530

    git commit

--- src/com/kazitanvir/Examples.java
+++ src/com/kazitanvir/Examples.java
@@ -5,10 +5,12 @@ public class Examples {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
+		print();
 
 	}
 
 	public static void print() {
+		System.out.println("I work in Publicis Sapient");
 
 	}
 
